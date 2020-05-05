e7d58b2a54cf81392b9449083412e4fc8a5ef8c0
shivam <shivamkumar116@gmail.com>
2020-05-05 10:36:15 +0530

    like feature

---
+++ LikeClass.ts
@@ -0,0 +1,32 @@
+export class Like {
+  private _isSelected: Boolean = false;
+  private _LikeCount: number;
+
+  constructor(theLikes: number) {
+    this.LikeCount = theLikes;
+  }
+
+  public get isSelected(): Boolean {
+    return this._isSelected;
+  }
+  public set isSelected(value: Boolean) {
+    this._isSelected = value;
+  }
+
+  public get LikeCount(): number {
+    return this._LikeCount;
+  }
+  public set LikeCount(value: number) {
+    this._LikeCount = value;
+  }
+
+  like(): void {
+    if (this.isSelected == false) {
+      this.isSelected = true;
+      this.LikeCount += 1;
+    } else if ((this.isSelected = true)) {
+      this.LikeCount -= 1;
+      this.isSelected = false;
+    }
+  }
+}
---
+++ LikeClassDriver.ts
@@ -0,0 +1,18 @@
+import { Like } from "./LikeClass";
+
+let likes = new Like(100);
+
+//pressing the button
+console.log("When user press the button..............");
+
+likes.like();
+console.log("Like Count : " + likes.LikeCount);
+console.log("Is selected : " + likes.isSelected);
+
+//again pressing the button
+
+console.log("When user again press the button..............");
+
+likes.like();
+console.log("Like Count : " + likes.LikeCount);
+console.log("Is selected : " + likes.isSelected);
